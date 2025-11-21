6430276beef0a50638e7ea0dcc43ff2d0811348e
Massimo-vdl <massimo.deluca@videotron.ca>
2025-11-21 10:46:09 -0500

    started GUI work

---
+++ src/finalproject_fall2025/FinalProject_Fall2025.java
@@ -0,0 +1,24 @@
+package finalproject_fall2025;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author massi
+ */
+public class FinalProject_Fall2025 extends Application{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        
+    }
+    
+}
