162cbfbe1c9d5e05b3489737af4e12b0b7ca2903
Your Name <matthew2006pereira@gmail.com>
2025-11-26 14:56:37 -0500

    fixed jfx library

--- src/finalproject_fall2025/FinalProject_Fall2025.java
+++ src/finalproject_fall2025/FinalProject_Fall2025.java
@@ -1,6 +1,9 @@
 package finalproject_fall2025;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 
